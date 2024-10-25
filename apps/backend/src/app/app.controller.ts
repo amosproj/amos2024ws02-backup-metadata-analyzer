7e0f235f9fa1f9423d3015dd0eac96355c39652e
Florian Oberndörfer <florian8751t@gmail.com>
2024-10-25 16:22:50 +0200

    Added hello endpoint for testing
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,8 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
 
 import { AppService } from "./app.service";
+import {ApiOperation} from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
+
+	@ApiOperation({
+		summary: "Dummy Endpoint for testing basic setup",
+	})
+	@Get('data')
+	getData() {
+		return this.appService.getData();
+	}
 }
--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from "@nestjs/common";
+import {Module} from "@nestjs/common";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+    imports: [TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5433,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'postgres',
+        entities: [],
+        synchronize: true,
+    }),],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
