b92440b44782bdf35a7ddb1d1fbfe2589cca4e0b
engelharddirk <dirk.engelhard@hotmail.de>
2024-10-25 19:01:25 +0200

    Migrate backend to ESBuild instead of webpack, use ESM Modules
    Signed-off-by: engelharddirk <dirk.engelhard@hotmail.de>

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'metadata-analyzer-backend',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/backend',
+	displayName: "metadata-analyzer-backend",
+	preset: "../../jest.preset.cjs",
+	testEnvironment: "node",
+	transform: {
+		"^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+	},
+	moduleFileExtensions: ["ts", "js", "html"],
+	coverageDirectory: "../../coverage/apps/backend",
 };
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,29 +3,30 @@
  * This is only a minimal backend to get started.
  */
 
-import {NestFactory} from "@nestjs/core";
+import { NestFactory } from "@nestjs/core";
 
-import {AppModule} from "./app/app.module";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { AppModule } from "./app/app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = "api";
-    app.setGlobalPrefix(globalPrefix);
+	const app = await NestFactory.create(AppModule);
+	const globalPrefix = "api";
+	app.setGlobalPrefix(globalPrefix);
 
-    const options = new DocumentBuilder()
-        .setTitle('Your API Title')
-        .setDescription('Your API description')
-        .setVersion('1.0')
-        .addServer('http://localhost:3000/', 'Local environment')
-        .addTag('Your API Tag')
-        .build();
+	const options = new DocumentBuilder()
+		.setTitle("Your API Title")
+		.setDescription("Your API description")
+		.setVersion("1.0")
+		.addServer("http://localhost:3000/", "Local environment")
+		.addTag("Your API Tag")
+		.build();
 
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api-docs', app, document);
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup("api-docs", app, document);
 
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
+	const port = process.env.PORT || 3000;
+	console.log("test");
+	await app.listen(port);
 }
 
 bootstrap();
