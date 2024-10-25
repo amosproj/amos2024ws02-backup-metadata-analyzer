d2d457d377e2bd20c88722c2c57a795add7ab47c
Florian Oberndörfer <florian8751t@gmail.com>
2024-10-25 14:45:20 +0200

    Typeorm install + Swagger Install + Config
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,17 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
+import {NestFactory} from "@nestjs/core";
 
-import { AppModule } from "./app/app.module";
+import {AppModule} from "./app/app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	const globalPrefix = "api";
-	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
-	await app.listen(port);
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = "api";
+    app.setGlobalPrefix(globalPrefix);
+
+    const options = new DocumentBuilder()
+        .setTitle('Your API Title')
+        .setDescription('Your API description')
+        .setVersion('1.0')
+        .addServer('http://localhost:3000/', 'Local environment')
+        .addTag('Your API Tag')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
 }
 
 bootstrap();
