c13ded25c3fc1f8c63541ab6c22d221e75d14a38
Christoph Klingenberg <christoph-klingenberg@web.de>
2024-10-27 22:29:57 +0100

    testing backend connection and clarity styles, helloWorld-versioin
    Signed-off-by: Christoph Klingenberg <christoph-klingenberg@web.de>

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HelloWorldService } from './statistics/service/hello-world.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'metadata-analyzer-frontend';
+  textarea: string = '---';
+
+  constructor(private readonly helloWorldService: HelloWorldService) {}
+
+  helloWorld() {
+    console.log('Hello WOrld!');
+    this.helloWorldService.getHelloWorld().subscribe({
+      next: (response) => (this.textarea = response),
+      error: (error) => console.error('Error fetching hello world:', error),
+    });
+  }
 }
--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from '@clr/angular';
 import { NgxEchartsModule } from 'ngx-echarts';
+import { HeaderComponent } from './ui/header/header.component';
+import { SidenavComponent } from './ui/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, HeaderComponent, SidenavComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ClarityModule,
     NgxEchartsModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
