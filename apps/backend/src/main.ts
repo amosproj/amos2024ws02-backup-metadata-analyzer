a96e0fcf252f8d095e6203ad10ab02ce1bc9ec8e
Christoph Klingenberg <christoph-klingenberg@web.de>
2024-10-27 22:28:55 +0100

    edit clarity design setup
    Signed-off-by: Christoph Klingenberg <christoph-klingenberg@web.de>

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,29 +3,36 @@
  * This is only a minimal backend to get started.
  */
 
-import {NestFactory} from "@nestjs/core";
+import { NestFactory } from '@nestjs/core';
 
-import {AppModule} from "./app/app.module";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cors = require('cors');
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = "api";
-    app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
 
-    const options = new DocumentBuilder()
-        .setTitle('Your API Title')
-        .setDescription('Your API description')
-        .setVersion('1.0')
-        .addServer('http://localhost:3000/', 'Local environment')
-        .addTag('Your API Tag')
-        .build();
+  app.use(
+    cors({
+      origin: 'http://localhost:4200', // Erlaubte URL des Angular-Frontends
+    })
+  );
 
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api-docs', app, document);
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('Your API Tag')
+    .build();
 
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 
 bootstrap();
