884a37efdd7462319f75728601a76febef09c37a
Florian Oberndörfer <florian8751t@gmail.com>
2024-10-27 21:48:32 +0100

    Setup for database migrations and interactions with typeorm
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,26 +1,19 @@
 import {Module} from '@nestjs/common';
-import {ConfigModule, ConfigService} from '@nestjs/config';
+import {ConfigModule} from '@nestjs/config';
 
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import {DbConfigService} from "./db-config.service";
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.getOrThrow<string>('DATABASE_HOST'),
-                port: +configService.getOrThrow<string>('DATABASE_PORT'),
-                username: configService.getOrThrow<string>('DATABASE_USER'),
-                password: configService.getOrThrow<string>('DATABASE_PASSWORD'),
-                database: configService.getOrThrow<string>('DATABASE_DATABASE'),
-                entities: [],
-                synchronize: true,
-            }),
-            inject: [ConfigService],
+            useClass: DbConfigService,
         }),
     ],
     controllers: [AppController],
