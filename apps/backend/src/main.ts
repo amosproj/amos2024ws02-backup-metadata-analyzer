bd376846f61e971b350a671c08aee22321556749
Florian Oberndörfer <florian8751t@gmail.com>
2024-10-28 10:41:22 +0100

    Use enableCors for allowing api requests from frontend
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,36 +3,33 @@
  * This is only a minimal backend to get started.
  */
 
-import { NestFactory } from '@nestjs/core';
+import {NestFactory} from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cors = require('cors');
+import {AppModule} from './app/app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
 
-  app.use(
-    cors({
-      origin: 'http://localhost:4200', // Erlaubte URL des Angular-Frontends
-    })
-  );
-  const options = new DocumentBuilder()
-    .setTitle('Your API Title')
-    .setDescription('Your API description')
-    .setVersion('1.0')
-    .addServer('http://localhost:3000/', 'Local environment')
-    .addTag('Your API Tag')
-    .build();
+    app.enableCors({
+        origin: 'http://localhost:4200', // Allowed URL of the Angular frontend
+    });
+    
+    const options = new DocumentBuilder()
+        .setTitle('Your API Title')
+        .setDescription('Your API description')
+        .setVersion('1.0')
+        .addServer('http://localhost:3000/', 'Local environment')
+        .addTag('Your API Tag')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
 
-  const port = process.env.PORT || 3000;
-  console.log('test');
-  await app.listen(port);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
 }
 
 bootstrap();
