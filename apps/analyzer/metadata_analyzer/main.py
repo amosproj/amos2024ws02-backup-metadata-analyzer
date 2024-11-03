90defa5854f3f5ecd59b946074570c2d79f17ba8
Amelie Regl <mellyre42@gmail.com>
2024-11-03 13:53:27 +0100

    Preparation for connection demo of analyzer to backend
    Signed-off-by: Amelie Regl <mellyre42@gmail.com>

--- apps/analyzer/metadata_analyzer/main.py
+++ apps/analyzer/metadata_analyzer/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, world!"
 
+@app.route("/echo", methods=['POST'])
+def echo():
+    if request.method == 'POST': 
+        data = request.get_json()
+        strData = data["input"]
+        newData = ""
+
+        for i in range (len(strData)-1,-1,-1):
+            newData = newData + strData[i]
+
+        return newData
 
 if __name__ == '__main__':
     app.run()
