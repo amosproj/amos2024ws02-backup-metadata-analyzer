80bd4f71f95b704587517e178c207bc894a611fa
Amelie Regl <mellyre42@gmail.com>
2024-11-04 16:42:32 +0100

    setup for environment variables
    Signed-off-by: Amelie Regl <mellyre42@gmail.com>

--- apps/analyzer/metadata_analyzer/main.py
+++ apps/analyzer/metadata_analyzer/main.py
@@ -1,6 +1,6 @@
-from flask import Flask,request
-import dotenv
-    
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
@@ -9,19 +9,24 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, world!"
 
-@app.route("/echo", methods=['POST'])
+
+@app.route("/echo", methods=["POST"])
 def echo():
-    if request.method == 'POST': 
+    if request.method == "POST":
         data = request.get_json()
         obj = data["body"]
         strData = obj["text"]
         newData = ""
 
-        for i in range (len(strData)-1,-1,-1):
+        for i in range(len(strData) - 1, -1, -1):
             newData = newData + strData[i]
-       
-        newBody = "{ \"output\": \"" + newData + "\" }"
+
+        newBody = '{ "output": "' + newData + '" }'
         return newBody
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    new_port = os.getenv("FLASK_RUN_PORT")
+    int_port = int(new_port or 5000)
+    print("int_port: " + str(int_port))
+    app.run(host="localhost", port=int_port, debug=False)
