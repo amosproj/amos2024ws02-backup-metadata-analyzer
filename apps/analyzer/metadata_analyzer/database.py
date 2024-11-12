c690c3d0a4416fc26beb32fa8f2ebcd4172299a0
heskil <93270171+heskil@users.noreply.github.com>
2024-11-12 17:53:46 +0100

    Update apps/analyzer/metadata_analyzer/database.py
    Co-authored-by: Dirk Engelhard <65366713+engelharddirk@users.noreply.github.com>

--- apps/analyzer/metadata_analyzer/database.py
+++ apps/analyzer/metadata_analyzer/database.py
@@ -5,20 +5,6 @@ from metadata_analyzer.models import BackupData
 import os
 
 
-""" class Database:
-    def __init__(self):
-        host = str(os.getenv("DATABASE_HOST") or "localhost")
-        port = int(os.getenv("DATABASE_PORT") or 5000)
-        user  = str(os.getenv("DATABASE_USER") or "postgres")
-        password = str(os.getenv("DATABASE_PASSWORD") or "postgres")
-        database = str(os.getenv("DATABASE_DATABASE") or "database")
-        
-        #url = f"postgresql+psycopg://{user}:{password}@{host}:{port}/{database}"
-        print("BUILT THIS URL")
-        print("postgresql+pg8000://"+user+":"+password+"@"+host+":"+str(port)+"/"+database)
-        url = "postgresql+pg8000://"+user+":"+password+"@"+host+":"+str(port)+"/"+database
-        self.engine = create_engine("postgresql+pg8000://"+user+":"+password+"@"+host+"/"+database,client_encoding='utf8')
-        """
 class Database:
     def __init__(self):
         self.engine = create_engine("postgresql+pg8000://postgres:postgres@localhost:5432/postgres")
