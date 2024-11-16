da7e7558ccb1f00a2f42c3711066e15785adce62
Oliver Schnell <mail@oliver-schnell.de>
2024-11-16 17:17:36 +0100

    Add model for results table
    Signed-off-by: Oliver Schnell <mail@oliver-schnell.de>

--- apps/analyzer/metadata_analyzer/models.py
+++ apps/analyzer/metadata_analyzer/models.py
@@ -17,3 +17,31 @@ class BackupData(Base):
 
     def __str__(self):
         return repr(self)
+
+class Result(Base):
+    __tablename__ = "results"
+
+    # For now I only added the most relevant columns
+    saveset: Mapped[str] = mapped_column(primary_key=True)
+    uuid: Mapped[str]
+    task: Mapped[str]
+    fdi_type: Mapped[str]
+    is_backup: Mapped[int]
+    state: Mapped[int]
+
+    start_time: Mapped[datetime]
+    stop_time: Mapped[datetime]
+    sbc_start: Mapped[datetime]
+
+    data_size: Mapped[int]
+    throughput: Mapped[str]
+    duration: Mapped[int]
+
+    def __repr__(self):
+        return f"""Result(uuid={self.uuid})"""
+
+    def __str__(self):
+        return repr(self)
+
+
+
