544bcc46fcbebd53d6a4c811e40859bdf1add06d
engelharddirk <dirk.engelhard@hotmail.de>
2024-11-26 13:44:55 +0100

    fix testDataComponentSpec
    Signed-off-by: engelharddirk <dirk.engelhard@hotmail.de>

--- apps/frontend/src/app/test-upload/component/find-test-data/find-test-data.component.spec.ts
+++ apps/frontend/src/app/test-upload/component/find-test-data/find-test-data.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BASE_URL } from '../../../shared/types/configuration';
 import { FindTestDataComponent } from './find-test-data.component';
+import { TestUploadServiceService } from '../../service/test-upload-service.service';
 
 describe('FindTestDataComponent', () => {
   let component: FindTestDataComponent;
@@ -8,7 +9,12 @@ describe('FindTestDataComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FindTestDataComponent]
+      declarations: [FindTestDataComponent],
+      providers: [
+        {provide: BASE_URL, useValue: TestUploadServiceService},
+        TestUploadServiceService
+      ]
+      
     })
     .compileComponents();
 
