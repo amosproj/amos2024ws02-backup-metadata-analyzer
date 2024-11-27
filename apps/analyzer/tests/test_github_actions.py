ea2f34df52bf3b2414105345562ceee77ff61484
Deniz Deli <d.deli@campus.tu-berlin.de>
2024-11-27 14:08:20 +0100

    Force false Test 3
    Signed-off-by: Deniz Deli <d.deli@campus.tu-berlin.de>

---
+++ apps/analyzer/tests/test_github_actions.py
@@ -0,0 +1,13 @@
+"""Main unit test module."""
+
+from metadata_analyzer.main import hello_world, update_data
+from metadata_analyzer.analyzer import Analyzer
+from metadata_analyzer.models import Result
+from metadata_analyzer.simple_rule_based_analyzer import SimpleRuleBasedAnalyzer
+from datetime import datetime
+from tests.mock_backend import MockBackend
+from tests.mock_database import MockDatabase
+
+def test_force_fail():
+    """Forced failing Test"""
+    assert False
