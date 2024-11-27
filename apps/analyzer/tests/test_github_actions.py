63f66181ee71ee4f5bcf1d172d76f12e00168362
Deniz Deli <d.deli@campus.tu-berlin.de>
2024-11-27 14:21:18 +0100

    Force false Test 4
    Signed-off-by: Deniz Deli <d.deli@campus.tu-berlin.de>

--- apps/analyzer/tests/test_github_actions.py
+++ apps/analyzer/tests/test_github_actions.py
@@ -8,10 +8,10 @@ from datetime import datetime
 from tests.mock_backend import MockBackend
 from tests.mock_database import MockDatabase
 
-# def test_force_fail():
-#     """Forced failing Test"""
-#     assert False
+def test_force_fail():
+    """Forced failing Test"""
+    assert False
 
-def test_force_pass():
-    """Forced passing Test"""
-    assert True
+# def test_force_pass():
+#     """Forced passing Test"""
+#     assert True
