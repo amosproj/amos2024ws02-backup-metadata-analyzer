20c4709fdb67e32ed38e4760ded474a3728f24c8
Amelie Regl <mellyre42@gmail.com>
2024-12-03 13:31:26 +0100

    fixed filtering of values and poetry setup
    Signed-off-by: Amelie Regl <mellyre42@gmail.com>

--- apps/analyzer/metadata_analyzer/timeSeriesTests.py
+++ apps/analyzer/metadata_analyzer/timeSeriesTests.py
@@ -2,6 +2,7 @@ import pandas as pd
 from darts import TimeSeries
 from sqlalchemy import create_engine
 from metadata_analyzer.database import Database
+from collections import Counter
 
 
 
@@ -13,16 +14,20 @@ with database.engine.connect() as conn, conn.begin():
     data_frame = pd.read_sql_table("results", conn)
 
 # preprocessing
+# removes null values in sbc_start and is_backup
+data_frame = data_frame[data_frame.sbc_start.notnull()]
+data_frame = data_frame[data_frame.is_backup.notnull()]
 # removes non-backups
-data_frame.drop(data_frame[data_frame['is_backup'] == 0].index)
+data_frame = data_frame[data_frame.is_backup != 0]
 # removes non-full backups (also removes copy backups in dataset for now)
-data_frame.drop(data_frame[data_frame['fdi_type'] == 'D'].index)
-data_frame.drop(data_frame[data_frame['fdi_type'] == 'I'].index)
-data_frame.drop(data_frame[data_frame['fdi_type'] == 'C'].index)
+# redo with only == 'F'
+data_frame = data_frame[data_frame.fdi_type != 'D']
+data_frame = data_frame[data_frame.fdi_type != 'I']
+data_frame = data_frame[data_frame.fdi_type != 'C']
+# remove entries that have the same sbc_start, necessary for indexing the time axis (could cause problems later)
+data_frame = data_frame.drop_duplicates(subset=['sbc_start'])
 
-    
-        
-    
 
-series = TimeSeries.from_dataframe(data_frame, 'sbc_start', 'data_size',fill_missing_dates=True,freq='None')
+
+series = TimeSeries.from_dataframe(data_frame, 'sbc_start', 'data_size',fill_missing_dates=True,freq='14s')
    
--- apps/analyzer/metadata_analyzer/time_series_analyzer.py
+++ apps/analyzer/metadata_analyzer/time_series_analyzer.py
@@ -15,12 +15,18 @@ class Time_series_analyzer:
             data_frame = pd.read_sql_table("results", conn)
 
         # preprocessing
+        # removes null values in sbc_start and is_backup
+        data_frame = data_frame[data_frame.sbc_start.notnull()]
+        data_frame = data_frame[data_frame.is_backup.notnull()]
         # removes non-backups
-        data_frame.drop(data_frame[data_frame['is_backup'] == 0].index)
+        data_frame = data_frame[data_frame.is_backup != 0]
         # removes non-full backups (also removes copy backups in dataset for now)
-        data_frame.drop(data_frame[data_frame['fdi_type'] == 'D'].index)
-        data_frame.drop(data_frame[data_frame['fdi_type'] == 'I'].index)
-        data_frame.drop(data_frame[data_frame['fdi_type'] == 'C'].index)
+        # redo with only == 'F'
+        data_frame = data_frame[data_frame.fdi_type != 'D']
+        data_frame = data_frame[data_frame.fdi_type != 'I']
+        data_frame = data_frame[data_frame.fdi_type != 'C']
+        # remove entries that have the same sbc_start, necessary for indexing the time axis (could cause problems later)
+        data_frame = data_frame.drop_duplicates(subset=['sbc_start'])
     
     def analyze_basic():
         # Time series analysis here
