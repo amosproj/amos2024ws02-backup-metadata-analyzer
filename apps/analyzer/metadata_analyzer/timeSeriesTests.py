bd75a8cfea48ca36832e0eeba892712f68f23613
Amelie Regl <mellyre42@gmail.com>
2024-12-03 14:00:10 +0100

    name refactoring
    Signed-off-by: Amelie Regl <mellyre42@gmail.com>

--- apps/analyzer/metadata_analyzer/timeSeriesTests.py
+++ apps/analyzer/metadata_analyzer/timeSeriesTests.py
@@ -11,23 +11,24 @@ database = Database()
 
 # read table into a dataframe
 with database.engine.connect() as conn, conn.begin():
-    data_frame = pd.read_sql_table("results", conn)
+    df = pd.read_sql_table("results", conn)
 
 # preprocessing
 # removes null values in sbc_start and is_backup
-data_frame = data_frame[data_frame.sbc_start.notnull()]
-data_frame = data_frame[data_frame.is_backup.notnull()]
+df = df[df.sbc_start.notnull()]
+df = df[df.is_backup.notnull()]
 # removes non-backups
-data_frame = data_frame[data_frame.is_backup != 0]
+df = df[df.is_backup != 0]
 # removes non-full backups (also removes copy backups in dataset for now)
 # redo with only == 'F'
-data_frame = data_frame[data_frame.fdi_type != 'D']
-data_frame = data_frame[data_frame.fdi_type != 'I']
-data_frame = data_frame[data_frame.fdi_type != 'C']
+df = df[df.fdi_type != 'D']
+df = df[df.fdi_type != 'I']
+df = df[df.fdi_type != 'C']
 # remove entries that have the same sbc_start, necessary for indexing the time axis (could cause problems later)
-data_frame = data_frame.drop_duplicates(subset=['sbc_start'])
+df = df.drop_duplicates(subset=['sbc_start'])
 
 
 
-series = TimeSeries.from_dataframe(data_frame, 'sbc_start', 'data_size',fill_missing_dates=True,freq='14s')
+
+series = TimeSeries.from_dataframe(df, 'sbc_start', 'data_size',fill_missing_dates=True,freq='D')
    
