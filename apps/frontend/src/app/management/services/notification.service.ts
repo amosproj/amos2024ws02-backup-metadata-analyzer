562b6b2d68306c3bed93fa7fe402a8dbb58c836b
Christoph Klingenberg <christoph-klingenberg@web.de>
2024-12-03 20:23:34 +0100

    fixed alert notification endpoint 4.o
    Signed-off-by: Christoph Klingenberg <christoph-klingenberg@web.de>

--- apps/frontend/src/app/management/services/notification.service.ts
+++ apps/frontend/src/app/management/services/notification.service.ts
@@ -20,7 +20,7 @@ export class NotificationService {
   updateNotificationSettings(notification: AlertType): Observable<AlertType> {
     return this.http.patch<AlertType>(
       `${this.baseUrl}/alerting/type/${notification.id}/user`,
-      { params: notification.user_active }
+      { status: notification.user_active }
     );
   }
 }
