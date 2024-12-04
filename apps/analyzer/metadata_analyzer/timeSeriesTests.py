1514acbd58b357b1f18cdbbd7906922d651e0dcc
Amelie Regl <mellyre42@gmail.com>
2024-12-04 12:22:25 +0100

    improved preprocessing as well as reducing interpolation
    Signed-off-by: Amelie Regl <mellyre42@gmail.com>

--- apps/analyzer/metadata_analyzer/timeSeriesTests.py
+++ apps/analyzer/metadata_analyzer/timeSeriesTests.py
@@ -11,10 +11,11 @@ database = Database()
 with database.engine.connect() as conn, conn.begin():
     df = pd.read_sql_table("results", conn)
 
-# preprocessing
-# removes null values in sbc_start and is_backup
+#--------------- General Preprocessing ---------------
+# removes null values in sbc_start, task_uuid and is_backup
 df = df[df.sbc_start.notnull()]
 df = df[df.is_backup.notnull()]
+df = df[df.task_uuid.notnull()]
 # removes non-backups
 df = df[df.is_backup != 0]
 #removes backups with size of 0.0
@@ -25,14 +26,20 @@ df = df[df.fdi_type == 'F']
 df = df.drop_duplicates(subset=['sbc_start'])
 # sorts dataframe by sbc_start
 df = df.sort_values('sbc_start')
+# gets all possible values for task_uuid
+task_ids = df['task_uuid'].unique()
 
+#--------------- Task Specific Preprocessing ---------------#
+# removes backups that do not have chosen task id; current task has frequency of 86402s
+df = df[df.task_uuid == task_ids[0]]
 # removes columns that are not relevant (change for other analyses)
 df = df[['sbc_start','data_size']]
+
 df.index = df['sbc_start'] # sets index to datetime in sbc_start column
 df = df.drop('sbc_start',axis=1) # removes column because values are now in index
 
-# resamples series to emulate backups at regular intervals, alternative: #new_df = df.resample('d').mean()
-df = df.asfreq('1D', method='pad')
+# interpolates series to emulate backups at regular intervals, different methods possible
+df = df.asfreq('86402s', method='ffill')
 
 
-series = TimeSeries.from_series(df,fill_missing_dates=True,freq=None)
+series = TimeSeries.from_series(df,fill_missing_dates=False,freq='86402s')
