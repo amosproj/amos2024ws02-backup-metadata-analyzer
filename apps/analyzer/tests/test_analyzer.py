0839b75e52be1647937dd1c32d5aa27544f44eda
Florian Oberndörfer <florian8751t@gmail.com>
2024-12-07 10:05:47 +0100

    Adjusted Analyzer Tests to include taskId
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/analyzer/tests/test_analyzer.py
+++ apps/analyzer/tests/test_analyzer.py
@@ -4,7 +4,7 @@ from metadata_analyzer.models import Result
 from tests.mock_backend import MockBackend
 from tests.mock_database import MockDatabase
 
-def _create_mock_result(task, uuid, fdi_type, data_size, start_time, is_backup=1):
+def _create_mock_result(task, uuid, fdi_type, data_size, start_time, task_uuid = None, is_backup=1):
     mock_result = Result()
     mock_result.task = task
     mock_result.uuid = uuid
@@ -12,16 +12,17 @@ def _create_mock_result(task, uuid, fdi_type, data_size, start_time, is_backup=1
     mock_result.data_size = data_size
     mock_result.start_time = start_time
     mock_result.is_backup = is_backup
+    mock_result.task_uuid = task_uuid
     return mock_result
 
 def test_update_data_all_types():
     mock_result1 = _create_mock_result("foo", "1", "F", 100_000_000, datetime.fromisoformat("2000-01-01"))
     mock_result2 = _create_mock_result("foo", "2", "D", 150_000_000, datetime.fromisoformat("2000-01-02"))
     mock_result3 = _create_mock_result("foo", "3", "I", 200_000_000, datetime.fromisoformat("2000-01-03"))
-    mock_result4 = _create_mock_result("foo", "4", "C", 250_000_000, datetime.fromisoformat("2000-01-04"))
+    mock_result4 = _create_mock_result("foo", "4", "C", 250_000_000, datetime.fromisoformat("2000-01-04"), '123')
     mock_results = [mock_result1, mock_result2, mock_result3, mock_result4]
 
-    database = MockDatabase(mock_results)
+    database = MockDatabase(mock_results, [])
     backend = MockBackend()
     Analyzer.init(database, backend, None, None)
     Analyzer.update_data()
@@ -30,28 +31,32 @@ def test_update_data_all_types():
         "id": mock_result1.uuid,
         "sizeMB": mock_result1.data_size / 1_000_000,
         "creationDate": mock_result1.start_time.isoformat(),
-        "type": "FULL"
+        "type": "FULL",
+        "taskId": None
     }, {
         "id": mock_result2.uuid,
         "sizeMB": mock_result2.data_size / 1_000_000,
         "creationDate": mock_result2.start_time.isoformat(),
-        "type": "DIFFERENTIAL"
+        "type": "DIFFERENTIAL",
+        "taskId": None
     }, {
         "id": mock_result3.uuid,
         "sizeMB": mock_result3.data_size / 1_000_000,
         "creationDate": mock_result3.start_time.isoformat(),
-        "type": "INCREMENTAL"
+        "type": "INCREMENTAL",
+        "taskId": None
     }, {
         "id": mock_result4.uuid,
         "sizeMB": mock_result4.data_size / 1_000_000,
         "creationDate": mock_result4.start_time.isoformat(),
-        "type": "COPY"
+        "type": "COPY",
+        "taskId": '123'
     }]
 
 def test_update_data_not_a_backup():
-    mock_result1 = _create_mock_result("foo", "1", "F", 100_000_000, datetime.fromisoformat("2000-01-01"), 0)
+    mock_result1 = _create_mock_result("foo", "1", "F", 100_000_000, datetime.fromisoformat("2000-01-01"), None, 0)
 
-    database = MockDatabase([mock_result1])
+    database = MockDatabase([mock_result1], [])
     backend = MockBackend()
     Analyzer.init(database, backend, None, None)
     Analyzer.update_data()
