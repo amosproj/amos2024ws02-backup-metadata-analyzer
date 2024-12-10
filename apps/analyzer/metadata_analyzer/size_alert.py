7f9269ea28e1e424ea78601aff43d54fd9676423
engelharddirk <dirk.engelhard@hotmail.de>
2024-12-10 11:42:22 +0100

    more review changes
    Signed-off-by: engelharddirk <dirk.engelhard@hotmail.de>

--- apps/analyzer/metadata_analyzer/size_alert.py
+++ apps/analyzer/metadata_analyzer/size_alert.py
@@ -1,7 +1,6 @@
 class SizeAlert:
     def __init__(self, result, reference_size):
         self.uuid = result.uuid
-        self.backup_saveset = result.saveset
         self.size = result.data_size / 1_000_000
         self.reference_size = reference_size / 1_000_000
         self.date = result.start_time
