335e207d8d8a452eee5e66e2b32ef8501dd7c509
Christoph Klingenberg <christoph-klingenberg@web.de>
2024-12-11 22:02:56 +0100

    add filter interface
    Signed-off-by: Christoph Klingenberg <christoph-klingenberg@web.de>

---
+++ apps/frontend/src/app/management/components/settings/email-receiver-settings/emailfilter.ts
@@ -0,0 +1,39 @@
+import { ClrDatagridFilterInterface } from '@clr/angular';
+import { EmailType } from 'apps/frontend/src/app/shared/types/email';
+import { Subject } from 'rxjs';
+
+export class CustomEmailFilter implements ClrDatagridFilterInterface<EmailType> {
+  public ranges: {
+    mail: string | null;
+    id: string | null;
+  } = {
+    mail: null,
+    id: null,
+  };
+
+  public changes = new Subject<any>();
+  public filterType: 'mail' | 'id';
+
+  constructor(filterType: 'mail' | 'id') {
+    this.filterType = filterType;
+  }
+
+  isActive(): boolean {
+    if (this.filterType === 'mail') {
+      return !!(this.ranges.mail || this.ranges.mail);
+    } else if (this.filterType === 'id') {
+      return !!(this.ranges.id || this.ranges.id);
+    } else {
+      return !!this.ranges.id;
+    }
+  }
+
+  accepts(backup: EmailType): boolean {
+    return true;
+  }
+
+  updateRanges(ranges: Partial<typeof this.ranges>): void {
+    Object.assign(this.ranges, ranges);
+    this.changes.next(true);
+  }
+}
