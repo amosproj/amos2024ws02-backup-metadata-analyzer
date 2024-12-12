ecc43246950ce949881152db7890ba9c97092810
Christoph Klingenberg <christoph-klingenberg@web.de>
2024-12-12 21:39:35 +0100

    add request error handling
    Signed-off-by: Christoph Klingenberg <christoph-klingenberg@web.de>

--- apps/frontend/src/app/management/services/email-receiver/email-receiver.service.ts
+++ apps/frontend/src/app/management/services/email-receiver/email-receiver.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@angular/core';
 import { BASE_URL } from '../../../shared/types/configuration';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { EmailType } from '../../../shared/types/email';
 
 @Injectable({
@@ -17,13 +17,32 @@ export class EmailReceiverService {
     return this.http.get<EmailType[]>(`${this.baseUrl}/mail`);
   }
 
-  updateEmailReceiver(email: EmailType): Observable<EmailType> {
-    return this.http.post<EmailType>(`${this.baseUrl}/mail`, {
-      mail: email.mail,
-    });
+  updateEmailReceiver(email: Partial<EmailType>): Observable<EmailType> {
+    return this.http
+      .post<EmailType>(`${this.baseUrl}/mail`, {
+        mail: email.mail,
+      })
+      .pipe(catchError(this.handleError));
   }
 
-  deleteEmail(email: EmailType): Observable<EmailType> {
-    return this.http.delete<EmailType>(`${this.baseUrl}/mail`);
+  deleteEmail(id: string): Observable<void> {
+    return this.http
+      .delete<void>(`${this.baseUrl}/mail/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An error occurred';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Server-side error
+      errorMessage =
+        error.status === 409
+          ? 'Email already exists'
+          : `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => error);
   }
 }
