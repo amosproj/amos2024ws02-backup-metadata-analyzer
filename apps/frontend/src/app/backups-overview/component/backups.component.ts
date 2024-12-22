7db7b54a5eafd3bf3f9db894457421654a88f3f0
Florian Oberndörfer <florian8751t@gmail.com>
2024-12-22 16:58:38 +0100

    readded taskcount for filter
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/frontend/src/app/backups-overview/component/backups.component.ts
+++ apps/frontend/src/app/backups-overview/component/backups.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { BehaviorSubject } from 'rxjs';
+import { BackupTask } from '../../shared/types/backup.task';
 
 @Component({
   selector: 'app-backups',
   templateUrl: './backups.component.html',
   styleUrl: './backups.component.css',
 })
-export class BackupsComponent {
+export class BackupsComponent implements AfterViewInit {
   protected filterPanel = false;
+
+  @ViewChild(SidePanelComponent) sidePanelComponent!: SidePanelComponent;
+  backupTaskSubject$: BehaviorSubject<BackupTask[]> | undefined;
+
+  ngAfterViewInit(): void {
+    this.backupTaskSubject$ = this.sidePanelComponent.backupTaskSubject$;
+  }
 
   /**
    * Change the state of the filter panel to open or close it
