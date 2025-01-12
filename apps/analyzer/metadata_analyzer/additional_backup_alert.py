c08998ab5166bd5220519b92ce93a3454c841cef
Oliver Schnell <mail@oliver-schnell.de>
2025-01-12 17:01:46 +0100

    Add new alerts as classes
    Signed-off-by: Oliver Schnell <mail@oliver-schnell.de>

---
+++ apps/analyzer/metadata_analyzer/additional_backup_alert.py
@@ -0,0 +1,10 @@
+class CreationDateAlert:
+    def __init__(self, result):
+        self.backup_uuid = result.uuid
+        self.date = result.start_time
+
+    def as_json(self):
+        return {
+                "backupId": self.backup_uuid,
+                "date": self.date.isoformat(),
+        }
