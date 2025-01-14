e22e07c1532b4e273c42c2b94f4d5742fc8a60ff
Deniz Deli <67391210+ddeli@users.noreply.github.com>
2025-01-14 09:06:12 +0100

    Update apps/frontend/src/app/backups-overview-page/component/data-stores-panel/data-stores.component.ts
    Co-authored-by: flo0852 <116088927+flo0852@users.noreply.github.com>

--- apps/frontend/src/app/backups-overview-page/component/data-stores-panel/data-stores.component.ts
+++ apps/frontend/src/app/backups-overview-page/component/data-stores-panel/data-stores.component.ts
@@ -70,8 +70,6 @@ export class DataStoresComponent implements OnDestroy, OnInit {
     if (overflowTime === 1) {
       return `${overflowTime} day`;
     } else if (overflowTime > 365) {
-      // const years = (overflowTime / 365).toFixed(1);
-      // return `${years} years`;
       return `> 1 year`;
     } else if (overflowTime == 0) {
       return `Now`;
