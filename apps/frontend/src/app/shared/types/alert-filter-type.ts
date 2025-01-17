58a6a6058c457565a2651c2f6ad077926c34ee39
Christoph Klingenberg <christoph-klingenberg@web.de>
2025-01-17 20:32:01 +0100

    add alert filter type
    Signed-off-by: Christoph Klingenberg <christoph-klingenberg@web.de>

---
+++ apps/frontend/src/app/shared/types/alert-filter-type.ts
@@ -0,0 +1,14 @@
+import { SeverityType } from "../enums/severityType";
+
+export type AlertFilterParams = {
+  limit?: number;
+  offset?: number;
+  orderBy?: string;
+  sortOrder?: 'ASC' | 'DESC';
+  severity?: SeverityType | null;
+  fromDate?: string | null;
+  toDate?: string | null;
+  id?: string | null;
+  alertType?: string[] | null;
+  includeDeprecated?: boolean | null
+};  
