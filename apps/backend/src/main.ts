7282596410c5e2e84559267b6c632c34529350c9
Florian Oberndörfer <florian8751t@gmail.com>
2025-01-17 22:09:00 +0100

    Added option to set logging level + set logging level of not so important logs to debug for performance improvement
    Signed-off-by: Florian Oberndörfer <florian8751t@gmail.com>

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,7 +10,7 @@ import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {logger: ['error', 'warn', 'log']});
     const globalPrefix = 'api';
     app.setGlobalPrefix(globalPrefix);
     app.useGlobalPipes(new ValidationPipe());
