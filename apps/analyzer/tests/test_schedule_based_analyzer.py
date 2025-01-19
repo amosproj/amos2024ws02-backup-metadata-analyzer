5089d79970910af4133db576347365454d701225
Oliver Schnell <mail@oliver-schnell.de>
2025-01-19 21:57:53 +0100

    Add more tests
    Signed-off-by: Oliver Schnell <mail@oliver-schnell.de>

--- apps/analyzer/tests/test_schedule_based_analyzer.py
+++ apps/analyzer/tests/test_schedule_based_analyzer.py
@@ -159,6 +159,122 @@ def test_alerts_correct_schedule():
     assert backend.missing_backup_alerts == []
     assert backend.additional_backup_alerts == []
 
+# Backups created according to the schedule should not generate alerts even with small deviations
+def test_alerts_correct_schedule_small_deviations():
+    mock_result1 = _create_mock_result(
+        "foo",
+        "1",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T12:00:00"),
+        "bar",
+    )
+    mock_result2 = _create_mock_result(
+        "foo",
+        "2",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T15:18:00"),
+        "bar",
+    )
+    mock_result3 = _create_mock_result(
+        "foo",
+        "3",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T17:42:00"),
+        "bar",
+    )
+    mock_result4 = _create_mock_result(
+        "foo",
+        "4",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T21:00:00"),
+        "bar",
+    )
+    mock_schedule = _create_mock_schedule("bar", "HOU", 3)
+    mock_task_event = _create_mock_task_event(1, "1", "foo", "bar")
+
+    database = MockDatabase(
+        [mock_result1, mock_result2, mock_result3, mock_result4],
+        [],
+        [],
+        [mock_schedule],
+        [mock_task_event],
+    )
+    backend = MockBackend()
+    schedule_based_analyzer = ScheduleBasedAnalyzer(backend)
+    Analyzer.init(database, backend, None, None, None, schedule_based_analyzer)
+    Analyzer.schedule_based_analysis(-1, datetime.fromisoformat("2000-01-01T22:00:00"))
+
+    assert backend.creation_date_alerts == []
+    assert backend.missing_backup_alerts == []
+    assert backend.additional_backup_alerts == []
+
+# Backups created according to the schedule should generate alerts with large deviations
+def test_alerts_correct_schedule_large_deviations():
+    mock_result1 = _create_mock_result(
+        "foo",
+        "1",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T12:00:00"),
+        "bar",
+    )
+    mock_result2 = _create_mock_result(
+        "foo",
+        "2",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T15:18:01"),
+        "bar",
+    )
+    mock_result3 = _create_mock_result(
+        "foo",
+        "3",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T17:41:59"),
+        "bar",
+    )
+    mock_result4 = _create_mock_result(
+        "foo",
+        "4",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T21:00:00"),
+        "bar",
+    )
+    mock_schedule = _create_mock_schedule("bar", "HOU", 3)
+    mock_task_event = _create_mock_task_event(1, "1", "foo", "bar")
+
+    database = MockDatabase(
+        [mock_result1, mock_result2, mock_result3, mock_result4],
+        [],
+        [],
+        [mock_schedule],
+        [mock_task_event],
+    )
+    backend = MockBackend()
+    schedule_based_analyzer = ScheduleBasedAnalyzer(backend)
+    Analyzer.init(database, backend, None, None, None, schedule_based_analyzer)
+    Analyzer.schedule_based_analysis(-1, datetime.fromisoformat("2000-01-01T22:00:00"))
+
+    assert backend.creation_date_alerts == [
+        {
+            "backupId": mock_result2.uuid,
+            "date": mock_result2.start_time.isoformat(),
+            "referenceDate": "2000-01-01T15:00:00",
+        },
+        {
+            "backupId": mock_result3.uuid,
+            "date": mock_result3.start_time.isoformat(),
+            "referenceDate": "2000-01-01T18:00:00",
+        },
+    ]
+    assert backend.missing_backup_alerts == []
+    assert backend.additional_backup_alerts == []
 
 # Additional backup should generate additional backup alert
 def test_alerts_additional_backup():
@@ -359,3 +475,67 @@ def test_alerts_multiple_missing_backups():
         },
     ]
     assert backend.additional_backup_alerts == []
+
+# Check correct behavior of mixing additional backup alerts and creation date alerts
+def test_alerts_creation_date_alert_and_additional_backup_alerts():
+    mock_result1 = _create_mock_result(
+        "foo",
+        "1",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T12:00:00"),
+        "bar",
+    )
+    mock_result2 = _create_mock_result(
+        "foo",
+        "2",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T15:00:00"),
+        "bar",
+    )
+    mock_result3 = _create_mock_result(
+        "foo",
+        "3",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T16:30:00"),
+        "bar",
+    )
+    mock_result4 = _create_mock_result(
+        "foo",
+        "4",
+        "F",
+        100_000_000,
+        datetime.fromisoformat("2000-01-01T20:00:00"),
+        "bar",
+    )
+    mock_schedule = _create_mock_schedule("bar", "HOU", 4)
+    mock_task_event = _create_mock_task_event(1, "1", "foo", "bar")
+
+    database = MockDatabase(
+        [mock_result1, mock_result2, mock_result3, mock_result4],
+        [],
+        [],
+        [mock_schedule],
+        [mock_task_event],
+    )
+    backend = MockBackend()
+    schedule_based_analyzer = ScheduleBasedAnalyzer(backend)
+    Analyzer.init(database, backend, None, None, None, schedule_based_analyzer)
+    Analyzer.schedule_based_analysis(-1, datetime.fromisoformat("2000-01-01T22:00:00"))
+
+    assert backend.creation_date_alerts == [
+        {
+            "backupId": mock_result3.uuid,
+            "date": mock_result3.start_time.isoformat(),
+            "referenceDate": "2000-01-01T16:00:00",
+        }
+    ]
+    assert backend.missing_backup_alerts == []
+    assert backend.additional_backup_alerts == [
+        {
+            "backupId": mock_result2.uuid,
+            "date": mock_result2.start_time.isoformat(),
+        }
+    ]
