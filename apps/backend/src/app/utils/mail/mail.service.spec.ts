b70bc84464c741d05d1c3ec59b54349dd4589ce6
engelharddirk <dirk.engelhard@hotmail.de>
2025-01-20 19:09:31 +0100

    fix tests broken due to deprecated field
    Signed-off-by: engelharddirk <dirk.engelhard@hotmail.de>

--- apps/backend/src/app/utils/mail/mail.service.spec.ts
+++ apps/backend/src/app/utils/mail/mail.service.spec.ts
@@ -137,6 +137,7 @@ describe('MailService', () => {
       },
       referenceDate: new Date(),
       creationDate: new Date(),
+      deprecated: false,
     };
 
     await service.sendAlertMail(alert);
